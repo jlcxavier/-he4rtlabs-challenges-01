729480411ffe49a7e1419a9569cb148f386bd6c8
José Luís <72719189+jlcxavier@users.noreply.github.com>
2021-07-16 16:56:59 -0300

    Upload FormattedScript
    Formatei o código do script que tava com tabs errados

--- src/main.js
+++ src/main.js
@@ -11,23 +11,25 @@ let objeto = {
 		var diasEfetivos = document.getElementById('diasEfetivos').value;
 		var diasFerias = document.getElementById('diasFerias').value;
 
-	if (valorProjeto.trim() === "" || 
-		horasDiarias.trim() === "" || 
-		diasEfetivos.trim() === "" || 
-		diasFerias.trim() === "" ) {			
+		if (
+			valorProjeto.trim() === "" || 
+			horasDiarias.trim() === "" || 
+			diasEfetivos.trim() === "" || 
+			diasFerias.trim() === "" 
+			) {			
 			alert("Você não preencheu uma das opções necessárias, por favor preencha todos os dados.");
 		} else {
-				valorProjeto = parseFloat(valorProjeto)
-				diasEfetivos = parseFloat(diasEfetivos)
-				horasDiarias = parseFloat(horasDiarias)
-				diasFerias = parseFloat(diasFerias)
+			valorProjeto = parseFloat(valorProjeto)
+			diasEfetivos = parseFloat(diasEfetivos)
+			horasDiarias = parseFloat(horasDiarias)
+			diasFerias = parseFloat(diasFerias)
 
-				let valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ( ( diasFerias * diasEfetivos * horasDiarias));
+			let valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ( ( diasFerias * diasEfetivos * horasDiarias));
 
 
-				var resultado = valorHora.toFixed(2)
+			var resultado = valorHora.toFixed(2)
 
-				document.getElementById('resultado').innerHTML = `R$ ${resultado}`;
-			}
+			document.getElementById('resultado').innerHTML = `R$ ${resultado}`;
+		}
 	}		
 }
