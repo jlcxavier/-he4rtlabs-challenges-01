741a1f6c2a0330123a7ac1238b16dbe74346b826
José Luís <72719189+jlcxavier@users.noreply.github.com>
2021-07-16 16:34:18 -0300

    Upload let objeto
    Criei um objeto e um erro para caso não forem preenchido todos os itens

--- src/main.js
+++ src/main.js
@@ -2,26 +2,32 @@
 //
 // By: danielhe4rt
 
+	
+
+let objeto = {
+	script() {
+		var valorProjeto = document.getElementById('valorProjeto').value;
+		var horasDiarias = document.getElementById('horasDiarias').value;
+		var diasEfetivos = document.getElementById('diasEfetivos').value;
+		var diasFerias = document.getElementById('diasFerias').value;
+
+	if (valorProjeto.trim() === "" || 
+		horasDiarias.trim() === "" || 
+		diasEfetivos.trim() === "" || 
+		diasFerias.trim() === "" ) {			
+			alert("Você não preencheu uma das opções necessárias, por favor preencha todos os dados.");
+		} else {
+				valorProjeto = parseFloat(valorProjeto)
+				diasEfetivos = parseFloat(diasEfetivos)
+				horasDiarias = parseFloat(horasDiarias)
+				diasFerias = parseFloat(diasFerias)
+
+				let valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ( ( diasFerias * diasEfetivos * horasDiarias));
 
 
-function calculo() {
-	var valorProjeto = document.getElementById('valorProjeto').value;
-	var horasDiarias = document.getElementById('horasDiarias').value;
-	var diasEfetivos = document.getElementById('diasEfetivos').value;
-	var diasFerias = document.getElementById('diasFerias').value;
+				var resultado = valorHora.toFixed(2)
 
-	valorProjeto = parseFloat(valorProjeto)
-	diasEfetivos = parseFloat(diasEfetivos)
-	horasDiarias = parseFloat(horasDiarias)
-	diasFerias = parseFloat(diasFerias)
-
-	let valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ( ( diasFerias * diasEfetivos * horasDiarias));
-
-	return valorHora.toFixed(2);
-}
-
-function imprimir() {
-	var resultado = calculo();
-
-	document.getElementById('resultado').innerHTML = `R$ ${resultado}`;
+				document.getElementById('resultado').innerHTML = `R$ ${resultado}`;
+			}
+	}		
 }
